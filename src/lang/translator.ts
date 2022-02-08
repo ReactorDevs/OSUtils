5f6f0b4f9bb10e6668b576edb7143a62eec0a811
ilaicraftYT <70978042+ilaicraftYT@users.noreply.github.com>
2022-02-08 17:40:04 -0600

    Initial commit x2

--- src/lang/translator.ts
+++ src/lang/translator.ts
@@ -1,3 +1,24 @@
+/*
+*   ___  ____  _   _ _   _ _     
+*  / _ \/ ___|| | | | |_(_) |___ 
+* | | | \___ \| | | | __| | / __|
+* | |_| |___) | |_| | |_| | \__ \
+*  \___/|____/ \___/ \__|_|_|___/
+*
+*
+* Copyright (C) 2021-2022  ilai
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*/
+
 export function translate(string){
 
 }
